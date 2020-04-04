3cd7c8c643a5ee60d162a885ceb7707f35b0a898
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-04 14:31:25 -0500

    tested createStockObject

---
+++ StockDatabaseApp/src/main/java/com/tobias/saul/jdbc/stock/pojos/Stock.java
@@ -0,0 +1,15 @@
+package com.tobias.saul.jdbc.stock.pojos;
+
+public class Stock {
+	
+	private long stockId;
+
+	public Stock(long stockId) {
+		this.stockId = stockId;
+	}
+
+	public long getStockId() {
+		return stockId;
+	}
+
+}
