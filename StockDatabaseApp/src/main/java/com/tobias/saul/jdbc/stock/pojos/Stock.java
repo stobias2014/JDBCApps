b79b605eabf5bde0b63539fb19c12bf904206bdd
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-04 14:54:56 -0500

    added fields to stock

--- StockDatabaseApp/src/main/java/com/tobias/saul/jdbc/stock/pojos/Stock.java
+++ StockDatabaseApp/src/main/java/com/tobias/saul/jdbc/stock/pojos/Stock.java
@@ -48,4 +48,43 @@ public class Stock {
 		this.price = price;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result + quantity;
+		result = prime * result + (int) (stockId ^ (stockId >>> 32));
+		result = prime * result + ((symbol == null) ? 0 : symbol.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Stock other = (Stock) obj;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		if (quantity != other.quantity)
+			return false;
+		if (stockId != other.stockId)
+			return false;
+		if (symbol == null) {
+			if (other.symbol != null)
+				return false;
+		} else if (!symbol.equals(other.symbol))
+			return false;
+		return true;
+	}
+	
+	
+
 }
